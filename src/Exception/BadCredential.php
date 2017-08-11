c29a52ac21efb9289f4496917f918e81904b2544
kemoycampbell <ksc2650@rit.edu>
2017-08-10 20:18:30 -0400

    allow developer to change output type

---
+++ src/Exception/BadCredential.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 8/10/17
+ * Time: 7:03 PM
+ */
+
+namespace hostjams\Cpanel\Exception;
+
+
+class BadCredential
+{
+
+}
---
+++ src/Exception/InvalidOutputType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 8/10/17
+ * Time: 4:25 PM
+ */
+
+namespace hostjams\Cpanel\Exception;
+
+
+class InvalidOutputType extends \RuntimeException
+{
+}
